bcf09366b6a4d666afb74962118ea85179d1504a
Iryna Kos <i.iryna.kos@gmail.com>
2020-05-12 11:25:31 +0200

    2lesson

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const numberOfFilms = +prompt(`Ile filmów oglądasz rocznie?`, ``);
-console.log(numberOfFilms);
+// console.log(numberOfFilms);
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -11,15 +11,42 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt(`Ostatni film, który oglądałeś?`, ``),
-      b = prompt(`Jak go oceniasz?`, ``),
-      c = prompt(`Ostatni film, który oglądałeś?`, ``),
-      d = prompt(`Jak go oceniasz?`, ``);
+// const a = prompt(`Ostatni film, który oglądałeś?`, ``),
+//       b = prompt(`Jak go oceniasz?`, ``),
+//       c = prompt(`Ostatni film, który oglądałeś?`, ``),
+//       d = prompt(`Jak go oceniasz?`, ``);
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++){
+    const a = prompt(`Ostatni film, który oglądałeś?`, ``),
+          b = prompt(`Jak go oceniasz?`, ``);
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done!');
+    } else {
+        console.log ('Error');
+        i--;
+    }  
+}
+
+if (personalMovieDB.count < 10) {
+    alert('Mało ogłądasz filmów')
+} else if (personalMovieDB.count == 10 || personalMovieDB.count < 30) {
+    alert('Jesteś super!')
+} else if (personalMovieDB.count > 30) {
+    alert('Za dużo! Nie przesadzaj!')
+} else {
+    alert('Coś poszło nie tak!')
+}
+
+
 
 console.log(personalMovieDB);
+
+
 
 
 
